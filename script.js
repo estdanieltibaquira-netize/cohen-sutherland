da82a8d7eb189f5702f7d38857405ec2b062653c
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-04-10 13:39:18 -0500

    Se crea la función calcularCodigo que determina en qué posición está un punto respecto al viewport usando códigos binarios

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ function dibujar() {
     ctx.lineTo(x1, y1);
     ctx.stroke();
 }
+// Códigos binarios
+const INSIDE = 0; // 0000
+const LEFT = 1;   // 0001
+const RIGHT = 2;  // 0010
+const BOTTOM = 4; // 0100
+const TOP = 8;    // 1000
+
+function calcularCodigo(x, y, xmin, ymin, xmax, ymax) {
+
+    let codigo = INSIDE;
+
+    if (x < xmin) codigo |= LEFT;
+    else if (x > xmax) codigo |= RIGHT;
+
+    if (y < ymin) codigo |= TOP;
+    else if (y > ymax) codigo |= BOTTOM;
+
+    return codigo;
+}
