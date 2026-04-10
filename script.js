00e67149d532db428acb10f1e883a5a07711838e
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-04-10 13:52:54 -0500

    Se integra el algoritmo de Cohen-Sutherland con el canvas para recortar y dibujar líneas dentro del viewport

--- script.js
+++ script.js
@@ -5,23 +5,16 @@ function dibujar() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // VIEWPORT (rectángulo visible)
     let xmin = 100, ymin = 100, xmax = 300, ymax = 250;
 
-    // Dibujar viewport
     ctx.strokeRect(xmin, ymin, xmax - xmin, ymax - ymin);
 
-    // Obtener puntos
     let x0 = parseInt(document.getElementById("x0").value);
     let y0 = parseInt(document.getElementById("y0").value);
     let x1 = parseInt(document.getElementById("x1").value);
     let y1 = parseInt(document.getElementById("y1").value);
 
-    // Dibujar línea sin recorte (por ahora)
-    ctx.beginPath();
-    ctx.moveTo(x0, y0);
-    ctx.lineTo(x1, y1);
-    ctx.stroke();
+    cohenSutherland(x0, y0, x1, y1, xmin, ymin, xmax, ymax, ctx);
 }
 // Códigos binarios
 const INSIDE = 0; // 0000
